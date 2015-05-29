bc8adc7975afbf54f36544160ec660be4d0fc1b2
wheatdog-nb-fedora <thenormalgod@gmail.com>
2015-05-29 22:58:19 +0800

    install-vim script

--- install-vim.sh
+++ install-vim.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
+cd ~
+
+TARGET=vimrc;
+
+if [ -e ".$TARGET" ] && [ ! -L ".$TARGET" ]; then
+    mv ".$TARGET" ".$TARGET.old"
+    echo "Backup your \"$TARGET\" to \"$HOME/.$TARGET.old\""
+fi
+if [ ! -L ".$TARGET" ]; then
+    ln -s "$HOME/.vim/$TARGET" ".$TARGET"
+    echo "Create a symbolic link \"$HOME/.vim/$TARGET\" to \"$HOME/.$TARGET\""
+fi
+
 VUNDLEDIR=~/.vim/bundle/Vundle.vim
 if [ ! -d "$VUNDLEDIR" ]; then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
 
+ln -fs ~/.vim/vimrc ~/.vimrc
